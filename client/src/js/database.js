e509be83ce7884f9ef05f605e5fe5d164972bf80
mjhwest89 <mjhwest89@gmail.com>
2022-01-20 15:36:11 +1030

    fixed error with database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,12 +16,12 @@ const initdb = async() =>
     });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async(id, content) => {
+export const putDb = async(content) => {
     console.log('PUT to the database');
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
-    const request = store.put({ id: id, todo: content });
+    const request = store.put({ value: content });
     const result = await request;
     console.log('🚀 - data saved to the database', result);
 }
@@ -37,7 +37,8 @@ export const getDb = async() => {
     const store = tx.objectStore('jate');
     // Use the .getAll() method to get all data in the database.
     const request = store.getAll();
-    // Get confirmation of the request.
+    console.log("")
+        // Get confirmation of the request.
     const result = await request;
     console.log('result.value', result);
     return result;
