9502a1663da9c302c249b87b3d5e5a3d80893ec2
mjhwest89 <mjhwest89@gmail.com>
2022-01-21 01:27:39 +1030

    added readme

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -39,11 +39,11 @@ export const getDb = async() => {
     // Use the .getAll() method to get all data in the database.
       const request = store.get(1);
     // const request = store.getAll();
-    // console.log("")
     // Get confirmation of the request.
     const result = await request;
     console.log('result.value', result);
     return result?.value;
+    // return result?.value;
 
 }
 
